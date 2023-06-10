f6be0b0be1c1fe4c1434168f37ec0a6183ca4998
emereum <24802844+emereum@users.noreply.github.com>
2023-06-10 11:05:18 +1000

    depends on mitmproxy

--- formula/http-tape-deck.rb
+++ formula/http-tape-deck.rb
@@ -4,6 +4,7 @@ class HttpTapeDeck < Formula
   url "https://github.com/emereum/http-tape-deck/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "65b770b2b8152e18a5a278f2d9b96ed8ff177d4121ecc899c02846d2afb48666"
   head "https://github.com/emereum/http-tape-deck.git"
+  depends_on "mitmproxy"
 
   def install
     # Move everything under #{libexec}/
