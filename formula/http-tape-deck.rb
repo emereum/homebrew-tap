906a71826a7d07a68926d1f6a457307f219cb61c
emereum <24802844+emereum@users.noreply.github.com>
2023-06-12 14:50:40 +1000

    http-tape-deck 1.0.2

--- formula/http-tape-deck.rb
+++ formula/http-tape-deck.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class HttpTapeDeck < Formula
   desc "HTTP traffic record and replay tool"
   homepage "https://github.com/emereum/http-tape-deck"
-  url "https://github.com/emereum/http-tape-deck/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "65b770b2b8152e18a5a278f2d9b96ed8ff177d4121ecc899c02846d2afb48666"
+  url "https://github.com/emereum/http-tape-deck/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "36a56e5fe749703daee777a1ae56416cb3972d23a7677814dc4f65b67ec42d13"
   head "https://github.com/emereum/http-tape-deck.git"
   depends_on "mitmproxy"
 
